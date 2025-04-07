3082c7d21cec4ff38f95d3cc1f45a2ab4cf069fe
jevinjosh <jevinjosh000@gmail.com>
2025-04-07 09:41:39 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+require('dotenv').config();
+const jwt=require('jsonwebtoken');
+const secret =process.env.SECRET;
 const encrypt = (payload, secret) => {
   // your code here and return token
+  const token =jwt.sign(payload,secret,{
+    expiresIn:'1h',
+    algorithm:'HS256',
+  });
+  console.log(token);
+  return token;
 };
 
 module.exports = encrypt;
